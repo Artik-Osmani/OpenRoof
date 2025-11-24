94ddf4880909e64c482338d531346847a72fbe05
Sumea <qungllasumea@gmail.com>
2025-11-24 22:22:59 +0100

    Problem

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Header from "./components/organisms/Header";
@@ -7,31 +7,30 @@ import logoImage from "../src/assets/Screenshot 2025-11-19 203208.png";
 import LoginPage from './components/pages/LoginPage';
 import Blog from './components/pages/Blog';
 import Properties from './components/pages/Properties';
+import { UserProvider } from './context/UserContext'; 
 
+function AppContent() {
   return (
     <div className="App">
-      <Router>
-        <Header
-          src={logoImage}
-          alt="Company Logo"
-          homeUrl="/"
-          aboutUrl="/about"
-          blogUrl='/blog'
-          propertiesUrl="/properties"
-          logInUrl='/logIn'
-        />
-
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/About' element={<About />} />
-          <Route path='/Blog' element={<Blog />} />
-          <Route path='/Properties' element={<Properties />} />
-          <Route path='/LogIn' element={<LoginPage />} />
-        </Routes>
-      </Router>
+      <Header
+        src={logoImage}
+        alt="Company Logo"
+        homeUrl="/"
+        aboutUrl="/about"
+        blogUrl='/blog'
+        propertiesUrl="/properties"
+        logInUrl='/login'
+      />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/properties' element={<Properties />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Routes>
     </div>
   );
-
+}
 
 function App() {
   return (
--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -4,5 +4,4 @@ export default function Blog() {
   return (
     <div>Blog</div>
   )
-
 }
